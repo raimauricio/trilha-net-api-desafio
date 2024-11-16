0a9b1f5553731f450eb12284df26776c119d2fd7
= <=>
2024-11-15 23:09:51 -0300

    Implementando método obter por id

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,10 +18,13 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+
+            // Buscar o Id no banco utilizando o EF
+            // Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            var tarefa = _context.Tarefas.Where(tarefa => tarefa.Id == id).FirstOrDefault();
+            if(tarefa == null) return NotFound();
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
